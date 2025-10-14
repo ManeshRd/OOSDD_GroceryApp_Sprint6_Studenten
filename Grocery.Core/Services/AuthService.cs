c492eab0ef994c6e9964b98ec1ef5ec0aec2c6a0
ManeshRD <dewnarainmanesh72@gmail.com>
2025-10-14 23:16:10 +0200

    UC19 Product in database
    Created a Add new product page and added workflow for pipeline

--- Grocery.Core/Services/AuthService.cs
+++ Grocery.Core/Services/AuthService.cs
@@ -7,15 +7,25 @@ namespace Grocery.Core.Services
     public class AuthService : IAuthService
     {
         private readonly IClientService _clientService;
+        private Client? _currentUser;
+
+        public Client? CurrentUser => _currentUser; 
         public AuthService(IClientService clientService)
         {
             _clientService = clientService;
         }
+
         public Client? Login(string email, string password)
         {
             Client? client = _clientService.Get(email);
             if (client == null) return null;
-            if (PasswordHelper.VerifyPassword(password, client.Password)) return client;
+
+            if (PasswordHelper.VerifyPassword(password, client.Password))
+            {
+                _currentUser = client; 
+                return client;
+            }
+
             return null;
         }
     }
